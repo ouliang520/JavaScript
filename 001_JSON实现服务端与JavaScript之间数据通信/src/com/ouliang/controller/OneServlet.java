323bb6aa11682ffe0cdf13fcbff5ac1d6854cb38
ousong <1393844930@qq.com>
2021-11-07 02:27:02 +0800

    Update OneServlet.java

--- 001_JSON实现服务端与JavaScript之间数据通信/src/com/ouliang/controller/OneServlet.java
+++ 001_JSON实现服务端与JavaScript之间数据通信/src/com/ouliang/controller/OneServlet.java
@@ -1,7 +1,5 @@
 package com.ouliang.controller;
 
-import com.ouliang.entity.student;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
@@ -9,9 +7,11 @@ import java.io.IOException;
 public class OneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-student stu =new student();
-request.setAttribute("key",stu);
-request.getRequestDispatcher("/index_.jsp").forward(request,response);
+        //不能直接传student类对象给javaScript
+        //可以通过拼接方式与反射机制传入
+        String str = "{\"sid\":10,\"sname\":\"mike\"}";
+        request.setAttribute("key", str);
+        request.getRequestDispatcher("/index_1.jsp").forward(request, response);
     }
 
     @Override
