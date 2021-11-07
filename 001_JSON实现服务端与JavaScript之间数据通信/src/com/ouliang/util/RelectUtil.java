a291d631e51fa189b215d9a7fd2955b3f6a95984
ousong <1393844930@qq.com>
2021-11-07 12:39:04 +0800

    json

---
+++ 001_JSON实现服务端与JavaScript之间数据通信/src/com/ouliang/util/RelectUtil.java
@@ -0,0 +1,43 @@
+package com.ouliang.util;
+
+import java.lang.reflect.Field;
+
+public class RelectUtil {
+    //作用:将任意类型对象内容转换为JSON格式字符串返回
+    //参数:一个高级引用类型对象 student对象, Dopt对象
+    public String jsonObject(Object obj) {
+
+
+        Class classFile = null;
+        Field freldArray[] = null;
+        StringBuffer str = null;
+        //获取当前对象隶属的class文件
+        classFile = obj.getClass();
+        //获得clss文件所有属性
+        freldArray = classFile.getDeclaredFields();
+        //获取当前对象所有属性的值
+        for (int i =0;i<freldArray.length;i++) {
+            Field field =freldArray[i];
+            field.setAccessible(true);//确保私有访问权限属性可以再class文件外使用
+            String fieldName = field.getName();//获取属性名称
+            Object value = null;
+            try {
+                value = field.get(obj);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            //将获得的属性及其值拼接为JSON格式字符串
+            str.append("\"");
+            str.append(fieldName);
+            str.append("\"");
+            str.append(value);
+            str.append("\"");//拼接结果     "sid":"10"
+            if(i<freldArray.length){
+                str.append(",");
+            }
+        }
+        return str.toString();
+    }
+}
+
+
---
+++ out/artifacts/001_JSONJavaScript_war_exploded/index_1.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: celestial
+  Date: 2021/11/7
+  Time: 1:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script type="text/javascript">
+        var stuObj =${requestScope.key};//从request作用域中得到学生对象
+        window.alert("学生编号 "+stuObj.sid+"学生姓名 "+stuObj.sname)
+    </script>
+</head>
+<body>
+
+</body>
+</html>
